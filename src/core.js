42ca36ba2f3e3609b954db68a1f689c9e4855936
Jeff Lee <jlee116057@gmail.com>
2017-12-07 13:14:12 -0800

    render example charts from core to view

--- src/core.js
+++ src/core.js
@@ -1,12 +1,15 @@
 // Core js
 var photographer = require("./photographer.js");
 const faceApi = require("./faceapi.js");
+const view = require("./view.js");
 
+window.addEventListener("load", () => {
+    view.renderCharts();
+});
 
 faceApi.AnalyzeImage("asdasd");
 
 // get chart objects from html and pass to view
-
 
 // create instance of photographer and pass it function to call
 photographer.watchCamera(1, data => {
