583a010bd949b4fabfce49629407aea91949b0c6
Jonathan Friesen <jonathan.friesen01@sap.com>
2017-12-07 10:36:15 -0800

    adding core and updating wrangler

---
+++ src/core.js
@@ -0,0 +1,5 @@
+// Core js
+
+import * as wrangler from '/wrangler.js';
+import * as faceapi from '/faceapi.js';
+import * as view from '/view.js';
--- src/wrangler.js
+++ src/wrangler.js
@@ -1,11 +1,6 @@
 /*
 * A wrangler file to retrieve data and make it meaningful
 */
-import * as Core from '/Core.js';
-
-// Retrieve data
-var retrieveData = function() {
-};
 
 // Do magic
 var wrangler = function() {
