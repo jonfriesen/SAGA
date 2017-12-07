e6018b9e16ecef4e8ab4e6fd0811fd5dfb0214fb
Asif Mammadov <asif.mammadov@sap.com>
2017-12-07 10:32:35 -0800

    Skeleton of wrangler.js
    workstream:

---
+++ src/wrangler.js
@@ -0,0 +1,14 @@
+/*
+* A wrangler file to retrieve data and make it meaningful
+*/
+import * as Core from '/Core.js';
+
+// Retrieve data
+var retrieveData = function() {
+};
+
+// Do magic
+var wrangler = function() {
+};
+
+export {wrangler};
