19f0a142b3d01d423caeeae9bf92dbd536e16a97
Jonathan Friesen <jonathan.friesen01@sap.com>
2017-12-07 14:14:29 -0800

    adding source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,6 @@ module.exports = {
     entry: ["./src/core.js","./src/view.js","./src/faceapi.js","./src/wrangler.js","./src/photographer.js",],
     output: {
       filename: "./dist/bundle.js"
-    }
+    },
+    devtool: 'eval-source-map'
   }
