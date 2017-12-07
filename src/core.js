0f704d9eb1302b626228c1b7f3615b39c54bb9f7
Jonathan Friesen <jonathan.friesen01@sap.com>
2017-12-07 10:42:28 -0800

    adding photographer

--- src/core.js
+++ src/core.js
@@ -3,10 +3,13 @@
 import * as wrangler from '/wrangler.js';
 import * as faceapi from '/faceapi.js';
 import * as view from '/view.js';
+import * as photographer from '/photographer.js';
 
 // get chart objects from html and pass to view
 
 
 // create instance of photographer and pass it function to call
-
+photographer.watchCamera(1, data => {
+    faceapi(data)
+})
 
---
+++ src/photographer.js
@@ -0,0 +1,8 @@
+
+
+function watchCamera(delay, callback) {
+
+    // picture is taken
+    // callback(the picture);
+
+}
