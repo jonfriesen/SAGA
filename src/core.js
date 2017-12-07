ae37ed7e2802d97cfe2911d179eb9dbf90977903
Jonathan Friesen <jonathan.friesen01@sap.com>
2017-12-07 10:38:33 -0800

    adding comments

--- src/core.js
+++ src/core.js
@@ -3,3 +3,10 @@
 import * as wrangler from '/wrangler.js';
 import * as faceapi from '/faceapi.js';
 import * as view from '/view.js';
+
+// get chart objects from html and pass to view
+
+
+// create instance of photographer and pass it function to call
+
+
