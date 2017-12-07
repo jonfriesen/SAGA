43a195c83bc907616cf0ae8a092e029a4b497bc3
Jonathan Friesen <jonathan.friesen01@sap.com>
2017-12-07 14:38:29 -0800

    fixing bad call bug

--- src/core.js
+++ src/core.js
@@ -7,12 +7,8 @@ window.addEventListener("load", () => {
     view.renderCharts();
 });
 
-faceApi.AnalyzeImage("asdasd");
-
-// get chart objects from html and pass to view
-
 // create instance of photographer and pass it function to call
 photographer.watchCamera(1, data => {
-    faceapi(data)
-})
+    faceApi.AnalyzeImage(data)
+});
 
