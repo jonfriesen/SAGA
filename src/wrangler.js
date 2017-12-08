7056e13238d246d1832032a6d9d0a4a36ffff7f0
Guo <albert.guo@sap.com>
2017-12-07 18:58:38 -0800

    add function header

--- src/wrangler.js
+++ src/wrangler.js
@@ -27,7 +27,10 @@ function wrangle(aResponses) {
   return aMagic;
 }
 
-
+/**
+ * data wrangler function
+ * @param response array from faceapi 
+ */ 
 function wrangleNew(aResponses) {
   if(!aResponses || !Array.isArray(aResponses) || aResponses.length === 0) {
     return null;
@@ -109,99 +112,6 @@ function getEmotionValue(oEmotion) {
   }
   return sEmotion;
 }
-
-// Sample aResponses:
-/*
-[
-  {
-    "faceId": "49d55c17-e018-4a42-ba7b-8cbbdfae7c6f",
-    "faceRectangle": {
-      "top": 131,
-      "left": 177,
-      "width": 162,
-      "height": 162
-    },
-    "faceAttributes": {
-      "smile": 0,
-      "headPose": {
-        "pitch": 0,
-        "roll": 0.1,
-        "yaw": -32.9
-      },
-      "gender": "female",
-      "age": 22.9,
-      "facialHair": {
-        "moustache": 0,
-        "beard": 0,
-        "sideburns": 0
-      },
-      "glasses": "NoGlasses",
-      "emotion": {
-        "anger": 0,
-        "contempt": 0,
-        "disgust": 0,
-        "fear": 0,
-        "happiness": 0,
-        "neutral": 0.986,
-        "sadness": 0.009,
-        "surprise": 0.005
-      },
-      "blur": {
-        "blurLevel": "low",
-        "value": 0.06
-      },
-      "exposure": {
-        "exposureLevel": "goodExposure",
-        "value": 0.67
-      },
-      "noise": {
-        "noiseLevel": "low",
-        "value": 0
-      },
-      "makeup": {
-        "eyeMakeup": true,
-        "lipMakeup": true
-      },
-      "accessories": [],
-      "occlusion": {
-        "foreheadOccluded": false,
-        "eyeOccluded": false,
-        "mouthOccluded": false
-      },
-      "hair": {
-        "bald": 0,
-        "invisible": false,
-        "hairColor": [
-          {
-            "color": "brown",
-            "confidence": 1
-          },
-          {
-            "color": "black",
-            "confidence": 0.87
-          },
-          {
-            "color": "other",
-            "confidence": 0.51
-          },
-          {
-            "color": "blond",
-            "confidence": 0.08
-          },
-          {
-            "color": "red",
-            "confidence": 0.08
-          },
-          {
-            "color": "gray",
-            "confidence": 0.02
-          }
-        ]
-      }
-    }
-  }
-]
-*/
 
 export {
   wrangle,
