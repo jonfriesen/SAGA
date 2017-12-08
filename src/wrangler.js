f7742ed5a4b05a0ed9f56974be8e791d13fad0b4
Guo <albert.guo@sap.com>
2017-12-07 16:32:51 -0800

    new data format with aggregatedAnalysis

--- src/wrangler.js
+++ src/wrangler.js
@@ -28,24 +28,70 @@ function wrangle(aResponses) {
 }
 
 
+function wrangleNew(aResponses) {
+  if(!aResponses || !Array.isArray(aResponses) || aResponses.length === 0) {
+    return null;
+  }
+  let data = {
+    aggregatedAnalysis: aResponses[0].faceAttributes.emotion,
+    time: (new Date()).getTime(),
+    persons: []
+  }
+  if(aResponses.length === 1) {
+    data.persons.push({
+      gender: aResponses[0].faceAttributes.gender,
+      age: aResponses[0].faceAttributes.age,
+      emotions: aResponses[0].faceAttributes.emotion,
+      feeling: getEmotionValue(aResponses[0].faceAttributes.emotion)
+    })
+  } else {
+    for (var i = 0; i < aResponses.length; i++) {
+      let oPerson = {
+        gender: aResponses[i].faceAttributes.gender,
+        age: aResponses[i].faceAttributes.age,
+        emotions: aResponses[i].faceAttributes.emotion,
+        feeling: getEmotionValue(aResponses[i].faceAttributes.emotion)
+      };
+      if(i > 0) {
+        for(let key in data.aggregatedAnalysis){
+          data.aggregatedAnalysis[key] += aResponses[i].faceAttributes.emotion[key];
+        }
+      }
+      data.persons.push(oPerson);
+    }
+    for(let key in data.aggregatedAnalysis){
+        data.aggregatedAnalysis[key] /= aResponses.length;
+    }
+  }
+  return data;
+}
+
 /*
-Sample Person
-oPerson = {
-  gender: "male",
-  age: 24,
-  emotions: {
-    "anger": 0,
-    "contempt": 0,
-    "disgust": 0,
-    "fear": 0,
-    "happiness": 0,
-    "neutral": 0.986,
-    "sadness": 0.009,
-    "surprise": 0.005
+Sample data - return value from wrangleNew()
+{
+  aggregatedAnalysis: {
+  
   },
-  feeling: "neutral",
+  persons: [
+    {
+      gender: "male",
+      age: 24,
+      emotions: {
+        "anger": 0,
+        "contempt": 0,
+        "disgust": 0,
+        "fear": 0,
+        "happiness": 0,
+        "neutral": 0.986,
+        "sadness": 0.009,
+        "surprise": 0.005
+      },
+      feeling: "neutral"
+    } // or more person objects
+  ],
   time: 1512686380559  // miliseconds
 }
+
 */
 
 /**
